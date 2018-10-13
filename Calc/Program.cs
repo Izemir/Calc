49d9784726a291c38209518c7f2f979096bef674
Izemir <chistevg.pro@gmail.com>
2018-10-14 00:07:38 +0300

    second commit

---
+++ Calc/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+
+
+        public static string input="112dd33";
+
+        static char[] parsing;
+
+        static string answer = "@";
+
+
+        static void Main(string[] args)
+        {
+
+            Calculate();
+
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+
+
+
+        
+
+        
+
+        
+
+        public static void Calculate()
+        {
+            // массив из инпута
+            parsing = input.ToCharArray();
+
+            //List<string> datalist = new List<string>();
+            //datalist.AddRange(input.Select(c => c.ToString()));
+
+            //лист из инпута
+            List<char> datalist = new List<char>();
+            datalist.AddRange(input);
+
+
+
+            answer = ParseAndCalc(datalist);
+            //answer.text = datalist.Count.ToString();
+            //answer.text = ParseAndCalc(parsing);
+        }
+
+        public static string ParseAndCalc(List<char> toParse)
+        {
+            List<char> result = new List<char>();
+
+            string b = "@!";
+
+
+            for (int i = 0; i < toParse.Count; i++)
+            {
+                if (toParse.Count != 0)
+                {
+                    if (char.IsDigit(toParse[i]))
+                    {
+                        result.Add(toParse[i]);
+                        toParse.RemoveAt(i);
+                        i--;
+                    }
+                    else
+                    {
+
+                        toParse.RemoveAt(i);
+                        b = ParseAndCalc(toParse);
+                        break;
+                    }
+                }
+
+
+            }
+
+
+            string a = new string(result.ToArray());
+            //b = new string(toParse.ToArray());
+            
+
+            return "S:" + a + ";Con:" + b;
+        }
+
+        public static bool IsCharDigit(char c)
+        {
+            return ((c >= '0') && (c <= '9'));
+        }
+    }
+
+}
